74d392e7f72802de1867ca142d0742385f677ff0
njange <jamesnjange80@gmail.com>
2024-06-30 22:00:20 +0300

    using axios

--- Week1/requestHandler.js
+++ Week1/requestHandler.js
@@ -1,7 +1,22 @@
-const requestHandler = (req, res) => {
+const axios = require('axios');
+
+const requestHandler = async (req, res) => {
+  if (req.url === '/data') { // Assuming you want to fetch data on a specific path, e.g., '/data'
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(response.data));
+    } catch (error) {
+      console.error(error);
+      res.statusCode = 500;
+      res.end('Error fetching data');
+    }
+  } else {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello, World!\n');
-  };
-  
-  module.exports = requestHandler;
+  }
+};
+
+module.exports = requestHandler;
