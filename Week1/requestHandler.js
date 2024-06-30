8745042bf4e8f8d750bf934a50fb633ab02a6774
njange <jamesnjange80@gmail.com>
2024-06-30 21:39:24 +0300

    request handler

---
+++ Week1/requestHandler.js
@@ -0,0 +1,7 @@
+const requestHandler = (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+  };
+  
+  module.exports = requestHandler;
