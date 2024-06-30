6b2cb6094a604d9c9b17d5a87df0ad71ef5fd515
njange <jamesnjange80@gmail.com>
2024-06-30 18:48:04 +0300

    task4 complete server setup

--- Week1/index.js
+++ Week1/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
